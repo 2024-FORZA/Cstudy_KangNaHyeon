3a5bd1de1708335196b44b0fcb71797500a7037b
NaHyeon647 <forzeu@sookmyung.ac.kr>
2024-04-07 23:55:26 +0900

    1_Lab02

---
+++ OneDrive/바탕 화면/포르자 과제/FORZA 과제/Project1/Project1/1_Lab02_1.c
@@ -0,0 +1,14 @@
+//11382
+#include <stdio.h>
+
+int main()
+{
+	long long A, B, C = 1;
+
+
+	scanf_s("%lld %lld %lld", &A, &B, &C);
+	long long sum = A + B + C;
+	printf("%lld\n", sum);
+
+	return 0;
+}
